37ba4b29d5399c77011ff3447cb7697934667621
Roman Shigapov <i3.bait@yandex.ru>
2022-10-10 22:38:36 +0700

    pw 14 email, pass and default in userSchema

--- models/user.js
+++ models/user.js
@@ -1,19 +1,35 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Исследователь',
   },
   avatar: {
+    type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return isEmail(value);
+      },
+      message: 'Пожалуйста, введите корректный email.',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
